aa834a06b1c7cf25470c0e96b83169b608bccf92
3LBirds <3LBirds@users.noreply.github.com>
2014-03-04 21:57:53 -0500

    Create StreamElement.java

---
+++ StreamElement.java
@@ -0,0 +1,33 @@
+
+public class StreamElement {
+
+	 private long timestamp; // unix timestamp
+	 private long seq; // sequence number
+	 private byte[] data; // this is the user data
+	 
+	 public StreamElement( long timestamp, long seq, byte[] data)
+	 {
+		 this.timestamp = timestamp;
+		 this.seq = seq;
+		 this.data = data;
+	 }
+	 
+	 public long getTimestamp()
+	 {
+		 return timestamp;
+	 }
+	 
+	 public long getSeq()
+	 {
+		 return seq;
+	 }
+	 
+	 public byte[] getData()
+	 {
+		 return data;
+	 }
+	 
+	 
+
+
+}
