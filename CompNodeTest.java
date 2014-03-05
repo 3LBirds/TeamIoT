67577518c9a8100428062bdf6d6007a17193c0d6
3LBirds <3LBirds@users.noreply.github.com>
2014-03-04 23:43:23 -0500

    Create CompNodeTest.java

---
+++ CompNodeTest.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+
+public class CompNodeTest {
+
+	public static void main(String[] args) throws IOException {
+    	StreamReceiver rec = new StreamReceiver();
+    	rec.openSocket(2014);
+    
+	}
+}
